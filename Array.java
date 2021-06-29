bf3cff84ad21b56499678cf94dd82f62c06e660e
Rojeen <rm1428@jagmail.southalabama.edu>
2021-06-29 00:26:53 -0500

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class Array
+    {
+    public static void main(String[] args)
+        {
+        int[] a = {10, 20, 30, 40, 50};
+
+        for (int i = a.length-1; i >=0; i--)
+            {
+            System.out.println(a[i]);
+            }
+        for (int i : a)
+            {
+            System.out.println(i);
+            }
+        }
+    }
